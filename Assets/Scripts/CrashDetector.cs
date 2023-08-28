d0afe75f5d0d28d59b6a1c208702fae8426b47d9
andrzejmasztalski <72304329+AndrzejMasztalski-git@users.noreply.github.com>
2023-08-28 18:47:07 +0200

    crash adjustments

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,11 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    [SerializeField] CircleCollider2D headCollider;
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(0);
-        Debug.Log("ITS OVER ANAKIN!!!");
+        if(collision.tag == "Ground")
+        {
+            SceneManager.LoadScene(0);
+            Debug.Log("ITS OVER ANAKIN!!!");
+        }
+        
     }
 }
